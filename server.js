e064949405497745611e4e8c89343630a9228724
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-01-19 13:20:37 +0100

    add routes for all messages, for a specific date and a specific ID

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
-import cors from "cors"
-import express from "express"
+import cors from "cors";
+import express from "express";
+import data from "./data.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  res.send("Hello Technigo!"); // TO DO: replace with documentation of your API using e.g. Express List Endpoints
 })
+
+// All messages
+app.get("/messages", (req, res) => {
+  res.json(data);
+});
+
+// Messages for a specific date
+app.get("/messages/date/:date", (req, res) => {
+  const date = req.params.date;
+  const messagesFromDate = data.filter((message) => message.createdAt.slice(0, 10) === date); // createdAt needs to match format "YYYY-MM-DD"
+  res.json(messagesFromDate);
+});
+
+// Message with a specific ID
+app.get("/message/id/:id", (req, res) => {
+  const id = req.params.id;
+  const messageOfId = data.filter((message) => message._id === id);
+  res.json(messageOfId);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
+
