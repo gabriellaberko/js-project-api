891c8b170eb08e45b9d835061d8aa18dede8ab83
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-02-03 08:39:53 +0100

    change filterCritera for minimum likes

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/thoughts", async (req, res) => {
 
     //Filter on minimum of likes
     if (minLikes) {
-      filterCriteria.$expr = { $gte: [{ $size: "$hearts" }, Number(minLikes)] }; //gte = greater than or equal to
+      filterCriteria[`hearts.${Number(minLikes) - 1}`] = { $exists: true }; //gte = greater than or equal to
     }
 
     /* --- Functionality for sorting --- */
